2230d8f9975ec6c8c97f4c28900e7495c8ee5088
Gowtham Raju Murududdi <gowthamrajum@gmail.com>
2026-08-02 18:18:17 -0500

    cantica 0.2.26

--- Casks/cantica.rb
+++ Casks/cantica.rb
@@ -4,15 +4,15 @@
 cask "cantica" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.2.25"
+  version "0.2.26"
 
   on_arm do
-    sha256 "1867396c74548a64595235152d24b794afbb184304d0ac642e8b456fb60981f3"
+    sha256 "648ca0f0cfb0716e432a8642e97362317468b8853d388136406982ccff6864e6"
 
     url "https://github.com/gowthamrajum/lumen-presenter/releases/download/v#{version}/Cantica-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "8f9b5655b1e3c5c394f11d7cf44d003e11b087a922ceb1e7fb937ba783c086fa"
+    sha256 "d34b5f9fba644d8a6c8964c9e801924e033394d69659ed5b2de7016790dca2ba"
 
     url "https://github.com/gowthamrajum/lumen-presenter/releases/download/v#{version}/Cantica-#{version}.dmg"
   end
